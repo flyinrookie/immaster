2629ecc5173d86bc80a07c731a6355f26827e23f
toc <toc@home>
2020-07-11 23:32:26 -0600

    fix preload proble:nature_d

--- game/world/tutorial/courtyard.c
+++ game/world/tutorial/courtyard.c
@@ -1,13 +1,13 @@
-// 标准户外环境模板 STD_OUTDOORS
-
-inherit STD_ROOM;
+inherit STD_OUTDOORS;
 
 protected void create()
 {
     ::create();
+    setArea(0,0,1,0);
     set("short", "院子");
     set("long", @LONG
 一个小小的院子，随意地扔着几件工具。
 LONG);
     set("outdoors", 1); // 设置户外
 }
+
