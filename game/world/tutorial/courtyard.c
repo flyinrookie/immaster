2e5cf4c21056811f7b62b5bdde8689f85207eaff
toc <toc@home>
2020-07-12 00:05:39 -0600

    modify rooms

--- game/world/tutorial/courtyard.c
+++ game/world/tutorial/courtyard.c
@@ -6,7 +6,7 @@ protected void create()
     setArea(0,0,1,0);
     set("short", "前院");
     set("long", @LONG
-一个小小的院子，随意地扔着几件工具。
+一个小小的院子，随意地扔着几件工具：斧头、锄头、铁镐和一根钓鱼竿。
 LONG);
     set("outdoors", 1); // 设置户外
 }
